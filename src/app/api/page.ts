63da36a6d6118805ca703a20f22e58a0321830d5
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2025-06-27 11:24:52 +0100

    fix get product page

--- src/app/api/page.ts
+++ src/app/api/page.ts
@@ -1,10 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { fetchShopify } from "@/lib/shopify/shopify";
-import { QUERY_GET_PRODUCTS } from "@/lib/shopify/product"
+import { shopifyFetch } from "@/lib/shopify";
+import { GET_ALL_PRODUCTS_FOR_SHOP_PAGE } from "@/lib/shopify/queries/product-queries";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const data = await fetchShopify(QUERY_GET_PRODUCTS, { first: 1 });
+    const data = await shopifyFetch({
+      query: GET_ALL_PRODUCTS_FOR_SHOP_PAGE,
+      variables: { first: 1 },
+    });
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: (err as Error).message });
