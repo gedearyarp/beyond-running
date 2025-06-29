28f1bff3b1672618d793ed1de95b5baa90f5d027
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2025-06-29 22:53:08 +0100

    fix bottom banner text

--- src/components/home/bottom-banner.tsx
+++ src/components/home/bottom-banner.tsx
@@ -10,9 +10,7 @@ export default function BottomBanner() {
       </div>
       <div className="absolute bottom-12 left-0 right-0 text-center">
         <p className="text-xs md:text-sm text-white max-w-2xl mx-auto px-8 font-avant-garde">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
-          dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-          suscipit lobortis nisl ut.
+        Beyond running is a running apparel brand based in jakarta. Each collection is built from a collective response of the running community, designed to seamlessly fit in a runner’s wardrobe and tested to suit all running purposes.
         </p>
       </div>
     </div>
